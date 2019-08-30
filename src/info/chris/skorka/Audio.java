c3d443a87050477288373c5247fcab04fb928b99
Christopher Skorka <30539703+ChrisSkorka@users.noreply.github.com>
2019-08-30 15:24:24 +1000

    init

---
+++ src/info/chris/skorka/Audio.java
@@ -0,0 +1,72 @@
+package info.chris.skorka;
+
+
+import java.nio.*;
+
+import org.lwjgl.openal.*;
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.ALC10.*;
+import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_filename;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.libc.LibCStdlib.free;
+
+public class Audio {
+
+    public Audio(){
+        String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
+        long device = alcOpenDevice(defaultDeviceName);
+
+        int[] attributes = {0};
+        long context = alcCreateContext(device, attributes);
+        alcMakeContextCurrent(context);
+
+        ALCCapabilities alcCapabilities = ALC.createCapabilities(device);
+        ALCapabilities alCapabilities = AL.createCapabilities(alcCapabilities);
+
+        if(alCapabilities.OpenAL10) {
+            //OpenAL 1.0 is supported
+        }
+
+
+
+        String fileName = "sound.ogg";
+
+        //Allocate space to store return information from the function
+        stackPush();
+        IntBuffer channelsBuffer = stackMallocInt(1);
+        stackPush();
+        IntBuffer sampleRateBuffer = stackMallocInt(1);
+
+        ShortBuffer rawAudioBuffer = stb_vorbis_decode_filename(fileName, channelsBuffer, sampleRateBuffer);
+
+        //Retreive the extra information that was stored in the buffers by the function
+        int channels = channelsBuffer.get();
+        int sampleRate = sampleRateBuffer.get();
+        //Free the space we allocated earlier
+        stackPop();
+        stackPop();
+
+        //Find the correct OpenAL format
+        int format = -1;
+        if(channels == 1) {
+            format = AL_FORMAT_MONO16;
+        } else if(channels == 2) {
+            format = AL_FORMAT_STEREO16;
+        }
+
+        //Request space for the buffer
+        int bufferPointer = alGenBuffers();
+
+        //Send the data to OpenAL
+        alBufferData(bufferPointer, format, rawAudioBuffer, sampleRate);
+
+        //Free the memory allocated by STB
+        free(rawAudioBuffer);
+
+
+        alcDestroyContext(context);
+        alcCloseDevice(device);
+    }
+
+
+}
